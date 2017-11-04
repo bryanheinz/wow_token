6e92607fe9b824ff979da7c814b54483f9467ece
bryan heinz <bryan@bryanheinz.com>
2017-11-04 00:32:53 -0500

    refactored code. updated for wowtoken.info's new URL/JSON scheme. added catchall push if errors happen.

--- wow_token_push.py
+++ wow_token_push.py
@@ -3,35 +3,39 @@ import json
 import urllib
 import httplib
 import urllib2
+from pprint import pprint
 
-class main(object):
-    def __init__(self):
-        self.push_price = 20000
-        self.key = ''
-        self.app_token = ''
+push_price = 180000 # minimum gold price before sending notification
+key = ''            # https://pushover.net key
+app_token = ''      # https://pushover.net app token
 
-    def token(self):
-        url = 'https://wowtoken.info/wowtoken.json'
-        page = urllib2.urlopen(url)
-        data = json.load(page)
-        page.close()
-        
-        self.gold = data['update']['NA']['formatted']['buy']
-        self.raw_gold = data['update']['NA']['raw']['buy']
-        self.date_stamp = data['update']['NA']['formatted']['updated']
-        
-        if self.raw_gold <= self.push_price:
-            self.pushover()
-            
-    def pushover(self):
-        conn = httplib.HTTPSConnection("api.pushover.net:443")
-        conn.request("POST", "/1/messages.json",
-          urllib.urlencode({
-            "token": self.app_token,
-            "user": self.key,
-            "message": "Buy %s" % self.gold,
-          }), { "Content-type": "application/x-www-form-urlencoded" })
-        conn.getresponse()
-            
-run = main()
-run.token()
+
+
+def token():
+    url = 'https://data.wowtoken.info/snapshot.json'
+    page = urllib2.urlopen(url)
+    data = json.load(page)
+    page.close()
+    
+    gold = data['NA']['formatted']['buy']
+    raw_gold = data['NA']['raw']['buy']
+    
+    if raw_gold <= push_price:
+        pushover("Buy - {0}".format(gold))
+    
+def pushover(msg):
+    conn = httplib.HTTPSConnection("api.pushover.net:443")
+    conn.request("POST",
+                "/1/messages.json",
+                urllib.urlencode({
+                    "token": app_token,
+                    "user": key,
+                    "message": msg}),
+                { "Content-type": "application/x-www-form-urlencoded" })
+    conn.getresponse()
+    
+
+try:
+    token()
+except:
+    pushover("wow_token_push exited with an error.")
