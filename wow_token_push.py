054ad8cabfe1dad66d54c447a6ca5d4fae8462b9
Bryan Heinz <online@bryanheinz.com>
2020-08-03 04:52:00 +0000

    Updated for Python 3, changed token info source to wowtokenprices.com

--- wow_token_push.py
+++ wow_token_push.py
@@ -1,40 +1,49 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import json
-import urllib
-import httplib
-import urllib2
+import urllib.parse
+import urllib.request
 
-push_price = 180000 # minimum gold price before sending notification
-key = ''            # https://pushover.net key
+
+# Adjustable Variables #
+push_price = 120000 # minimum gold price before sending notification
+user_key = ''            # https://pushover.net key
 app_token = ''      # https://pushover.net app token
+# Adjustable Variables #
 
 
-
+# Main Code #
 def token():
-    url = 'https://data.wowtoken.info/snapshot.json'
-    page = urllib2.urlopen(url)
-    data = json.load(page)
-    page.close()
+    url = 'https://wowtokenprices.com/current_prices.json'
+    with urllib.request.urlopen(url) as page:
+        encoding = page.info().get_content_charset('utf-8')
+        data = page.read()
     
-    gold = data['NA']['formatted']['buy']
-    raw_gold = data['NA']['raw']['buy']
+    data = json.loads(data.decode(encoding))
     
-    if raw_gold <= push_price:
-        pushover("Buy - {0}".format(gold))
+    gold = data['us']['current_price']
+    
+    if gold <= push_price:
+        pushover("Buy - {:,}".format(gold))
     
 def pushover(msg):
-    conn = httplib.HTTPSConnection("api.pushover.net:443")
-    conn.request("POST",
-                "/1/messages.json",
-                urllib.urlencode({
-                    "token": app_token,
-                    "user": key,
-                    "message": msg}),
-                { "Content-type": "application/x-www-form-urlencoded" })
-    conn.getresponse()
+    url = 'https://api.pushover.net:443/1/messages.json'
     
+    headers = {
+        'Content-type':'application/x-www-form-urlencoded',
+    }
+    
+    data = urllib.parse.urlencode({
+        'token':app_token,
+        'user':user_key,
+        'message':msg
+    }).encode('utf-8')
+    
+    req = urllib.request.Request(url, headers=headers, data=data)
+    urllib.request.urlopen(req).close()
+
 
 try:
     token()
-except:
+except Exception as e:
+    print(e)
     pushover("wow_token_push exited with an error.")
