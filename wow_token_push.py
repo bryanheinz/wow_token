e60d21c44c78374d495f1f40c107f087c6028928
Bryan Heinz <online@bryanheinz.com>
2020-10-09 02:42:27 +0000

    added fallback to 1-day history if current prices isn't returning data.

--- wow_token_push.py
+++ wow_token_push.py
@@ -5,26 +5,36 @@ import urllib.request
 
 
 # Adjustable Variables #
-push_price = 120000 # minimum gold price before sending notification
-user_key = ''            # https://pushover.net key
-app_token = ''      # https://pushover.net app token
+push_price = 150000    # minimum gold price before sending notification
+user_key = ''          # https://pushover.net key
+app_token = ''         # https://pushover.net app token
 # Adjustable Variables #
 
 
 # Main Code #
 def token():
-    url = 'https://wowtokenprices.com/current_prices.json'
-    with urllib.request.urlopen(url) as page:
-        encoding = page.info().get_content_charset('utf-8')
-        data = page.read()
+    data, encoding = fetch_data('https://wowtokenprices.com/current_prices.json')
     
-    data = json.loads(data.decode(encoding))
-    
-    gold = data['us']['current_price']
+    try:
+        json_data = json.loads(data.decode(encoding))
+        gold = data['us']['current_price']
+    except json.decoder.JSONDecodeError:
+        # fallback if current_prices isn't returning data
+        data, encoding = fetch_data(
+            'https://wowtokenprices.com/history_prices_1_day.json'
+        )
+        json_data = json.loads(data.decode(encoding))
+        gold = json_data['us'][-1]['price']
     
     if gold <= push_price:
         pushover("Buy - {:,}".format(gold))
-    
+
+def fetch_data(url):
+    with urllib.request.urlopen(url) as page:
+        encoding = page.info().get_content_charset('utf-8')
+        data = page.read()
+    return(data, encoding)
+
 def pushover(msg):
     url = 'https://api.pushover.net:443/1/messages.json'
     
@@ -40,7 +50,6 @@ def pushover(msg):
     
     req = urllib.request.Request(url, headers=headers, data=data)
     urllib.request.urlopen(req).close()
-
 
 try:
     token()
